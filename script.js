f13f9280ce937fca952c45be0a46374f5e7ef06e
Love Olubummo <olubummolove3@gmail.com>
2023-02-15 18:53:32 +0100

    feature: created startclock and setRotation function

---
+++ script.js
@@ -0,0 +1,21 @@
+const hourHand = document.querySelector("[data-hour-hand]");
+const minuteHand = document.querySelector("[data-minute-hand]");
+const secondHand = document.querySelector("[data-second-hand]");
+
+function startClock() {
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hourRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hourRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotate", rotationRatio * 360);
+}
+
+startClock();
+
+setInterval(startClock, 1000);
